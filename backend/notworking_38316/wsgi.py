8a91c2fb9043e62678bb894007eae6a5d9f49fc7
Crowdbotics <team@crowdbotics.com>
2023-01-16 15:32:44 +0000

    Initial commit.

---
+++ backend/notworking_38316/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for notworking_38316 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notworking_38316.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "notworking-38316",
+  url: "https://notworking-38316.botics.co",
+  api: "https://notworking-38316.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
